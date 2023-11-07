46ea9807ae7a5b44b04c2abddcf5d5f446664622
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-11-07 11:25:27 +1000

    more general solution for bad extensionFlag

--- src/nifti1.ts
+++ src/nifti1.ts
@@ -383,12 +383,18 @@ import { Utils } from "./utilities";
 
       this.isHDR = this.magic === String.fromCharCode.apply(null, NIFTI1.MAGIC_NUMBER2);
 
-      if (rawData.byteLength > NIFTI1.MAGIC_COOKIE) {
+      let isExtensionCapable = (rawData.byteLength > NIFTI1.MAGIC_COOKIE);
+      if ((!this.isHDR) && (this.vox_offset <= 352))
+        isExtensionCapable = false;
+      if (rawData.byteLength < (352 + 16))
+        isExtensionCapable = false;
+
+      if (isExtensionCapable) {
         this.extensionFlag[0] = Utils.getByteAt(rawData, 348);
         this.extensionFlag[1] = Utils.getByteAt(rawData, 348 + 1);
         this.extensionFlag[2] = Utils.getByteAt(rawData, 348 + 2);
         this.extensionFlag[3] = Utils.getByteAt(rawData, 348 + 3);
-        if (this.vox_offset > 352 && this.extensionFlag[0]) {
+        if (this.extensionFlag[0]) {
           // read our extensions
           this.extensions = Utils.getExtensionsAt(
             rawData,
